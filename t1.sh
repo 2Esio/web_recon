39218b139f60d62e6059ef6f4b461b8cb9c60f6b
2Esio <marco_flores_cid@ciencias.unam.mx>
2026-09-10 18:04:43 -0600

    Se agrega variable global para sacar informacion y pasarlas a grep | 2Esio | 10-09-2026

--- t1.sh
+++ t1.sh
@@ -1,21 +1,12 @@
 #/bin/bash
 
-ip=$1
-#ping= ping $1
-#hostname= whois $1  | grep 'name' & 'Name' & 'Nombre' 
-domain_name=$(whois $1 | grep 'Domain Name')
-created_on=$(whois $1 | grep 'Created On:')
-expiration_date=$(whois $1 | grep 'Expiration Date:')
-url=$(whois $1 | grep 'URL:')
-
-
+whois_info=$(whois "$1")
+created_on=$(echo "$whois_info" | grep 'Created On:')
 
 echo "
 ====================
-Objetive: $ip 
-$created_on
-$expiration_date
-$url
+Objetive: $1
+Created On: $created_on
 Contact Name:
 City:
 State:
